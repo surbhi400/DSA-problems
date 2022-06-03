79babc015ed2cae06ef4bf2e92378e3cac9a67dd
Surbhi Kumari <77582783+surbhi400@users.noreply.github.com>
2022-06-03 22:30:21 +0530

    added the code of searching in an array

---
+++ Array/Searching in an array where adjacent differ by at most k.cpp
@@ -0,0 +1,10 @@
+int search(int arr[], int n, int x, int k)
+{
+    // Complete the function
+    for(int i=0; i<n; i++){
+        if(arr[i]==x){
+            return i;
+        }
+    }
+    return -1;
+}    	
