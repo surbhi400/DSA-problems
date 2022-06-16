5b5c9bad13f21ae4bd167ba9db16e0f63cb1aac0
Surbhi Kumari <77582783+surbhi400@users.noreply.github.com>
2022-06-16 12:11:34 +0530

    Adding code to print elements of a linked list.

---
+++ Linked list/Print Linked List elements .cpp
@@ -0,0 +1,12 @@
+class Solution
+{
+    public:
+    void display(Node *head)
+    {
+      //your code goes here
+      while(head){
+          cout<<head->data<<" ";
+          head=head->next;
+      }
+    }
+};
