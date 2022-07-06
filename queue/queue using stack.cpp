10491997a1fbcc983cbeaf84ff7ad03dfd7e3eea
Surbhi Kumari <77582783+surbhi400@users.noreply.github.com>
2022-07-06 09:25:12 +0530

    Added code to solve queue using stack.

---
+++ queue/queue using stack.cpp
@@ -0,0 +1,35 @@
+class Queue {
+    stack<int> input, output;
+public:
+
+    void enqueue(int x) {
+        input.push(x);
+    }
+    
+
+    int dequeue() {
+        
+        //if(input.empty()){
+            //return -1;
+        //}
+        //else{
+            while(!input.empty())
+        {
+            output.push(input.top());
+            input.pop();
+        
+        }
+        int ans = output.top();
+        output.pop();
+            
+        while(!output.empty())
+        {
+            input.push(output.top());
+            output.pop();
+            
+        }
+        
+        //}
+        return ans;
+    }
+};
