9ea9d4867ed50e9c97e7f68f1aabc9cd1bbab3e7
Surbhi Kumari <77582783+surbhi400@users.noreply.github.com>
2022-07-24 00:05:50 +0530

    Top View of Binary Tree.

--- Tree/Top View of Binary Tree.cpp
+++ Tree/Top View of Binary Tree.cpp
@@ -10,23 +10,28 @@
 // Expected Time Complexity: O(N)
 // Expected Auxiliary Space: O(N).
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-vector<int> topView(Node *root)
+class Solution
+{
+    public:
+    //Function to return a list of nodes visible from the top view 
+    //from left to right in Binary Tree.
+    vector<int> topView(Node *root)
     {
         //Your code here
         vector<int> ans;
         if(root==NULL){
             return ans;// if tree is empty then return empty vector.
         }
-        queue<pair<Node*, int>> q;
-        map<int,int> mpp;
-        q.push({root,0});
+        queue<pair<Node*, int>> q;// takes a queue which stores node and its axis.
+        map<int,int> mpp;// take a map which stores the value of node and line.
+        q.push({root,0});// push the root node and the axis on which it is present.
         while(!q.empty()){
             auto it=q.front();// gives topmost node
             q.pop();
             Node* node=it.first;
             int line=it.second;// gives the line.
             
-            if(mpp.find(line)==mpp.end()) mpp[line]=node->data; // if the line is not present on map.
+            if(mpp.find(line)==mpp.end()) mpp[line]=node->data;// if the line is not present in the map.
             if(node->left!=NULL){
                 q.push({node->left,line-1});
             }
@@ -36,9 +41,9 @@ vector<int> topView(Node *root)
             }
         }
         for(auto it:mpp){
-            ans.push_back(it.second); // push the elements of the map in the vector.
+            ans.push_back(it.second);// push the elements from the map to the resultant vector.
         }
-        return ans;// return vector.
+        return ans;// return resultant vector.
     }
 
 };
