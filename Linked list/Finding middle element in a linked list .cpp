69a900d4448cf7632f4b05ce5673ffd3e6e6c6ed
Surbhi Kumari <77582783+surbhi400@users.noreply.github.com>
2022-08-20 12:42:59 +0530

    Finding middle element in a linked list.

--- Linked list/Finding middle element in a linked list .cpp
+++ Linked list/Finding middle element in a linked list .cpp
@@ -1,7 +1,26 @@
-int getMiddle(Node *head)
+/* Given a singly linked list of N nodes.
+The task is to find the middle of the linked list. For example, if the linked list is
+1-> 2->3->4->5, then the middle node of the list is 3.
+If there are two middle nodes(in case, when N is even), print the second middle element.
+For example, if the linked list given is 1->2->3->4->5->6, then the middle node of the list is 4.
+
+Example 1:
+
+Input:
+LinkedList: 1->2->3->4->5
+Output: 3 
+Explanation: 
+Middle of linked list is 3.
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(1).*/
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+class Solution{
+    public:
+    /* Should return data of middle node. If linked list is empty, then  -1*/
+    int getMiddle(Node *head)
     {
-        // Your code here
-  // we will find the length of the linked list.
+        //Your code here
         Node* temp=head;
         int length=0;
         while(temp!=NULL){
@@ -18,6 +37,21 @@ int getMiddle(Node *head)
             
             temp=temp->next;
         }
-        return temp->data;
+        return temp->data
+        
+                or
+        
+        Node* fast=head;
+        Node* slow=head;
+        while(fast!=NULL && fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        return slow->data;
     }
+    
+    
 };
+                                                         
+                                                              
+                                                              
